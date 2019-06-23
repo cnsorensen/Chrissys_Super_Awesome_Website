c9d5ff98d3d60594508eb9a652820a51fa9b9b3d
cnsorensen <cnsorensen0@gmail.com>
2019-06-23 14:58:09 -0400

    Added CCA page and php functionality. Major indent repair. Added comments. Deleted pictures in favor of using links instead.

---
+++ poll.php
@@ -0,0 +1,22 @@
+<?php
+// open a file based on the voter's name
+$filename = $_POST['m_name'] . ".txt";
+$file = fopen($filename, "w");
+
+// DEBUG
+echo $filename;
+
+// write the votes
+// Write voter's name
+fwrite($file, $_POST['m_name'] . "\n");
+
+// Little Mermaid votes
+fwrite($file, "The Little Mermaid\n");
+fwrite($file, $_POST['m_ariel'] . "\n");
+fwrite($file, $_POST['m_eric'] . "\n");
+fwrite($file, $_POST['m_ursula'] . "\n");
+
+// close file
+fclose($file);
+// good boy :)
+?>
