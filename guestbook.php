7f60e972c0835e5190c0fe33c6ab91ed200e9d18
cnsorensen <cnsorensen0@gmail.com>
2020-02-14 13:42:10 -0500

    Oofta, failed to do a code cleanup before pushing

--- guestbook.php
+++ guestbook.php
@@ -1,19 +1,12 @@
 <?php
-// time
-//$todaydate = date("H.i.s");
-// date
-//date("Y-m-d");
 
-// open a file based on the poster's name
+// open file based on date
 $filename = "guestbook/post_" . date("Ymd") . "_" .  date("his") . ".txt";
-//$filename = "BOB.txt";
 $file = fopen($filename, "w");
 
-// write the votes
 // Write posters's name and comments
 fwrite($file, $_POST['m_name'] . "\n");
 fwrite($file, $_POST['m_comments']);
-//fwrite($file, "\nbobobobobo");
 
 // close file
 fclose($file);
